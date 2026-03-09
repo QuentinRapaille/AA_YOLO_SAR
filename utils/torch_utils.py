24bf68fca77c9af88f8e53dd4d46953a1d792e8a
Quentin Rapaille <quentin.rapaille@ens-paris-saclay.fr>
2026-03-09 10:38:34 +0100

    Debug

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -16,8 +16,6 @@ import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-import traceback
-
 try:
     import thop  # for FLOPS computation
 except ImportError:
@@ -214,14 +212,14 @@ def model_info(model, verbose=False, img_size=640):
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
 
     try:  # FLOPS
-        from thop import profile
+        if thop is None:
+            raise ImportError('thop is not available')
         stride = max(int(model.stride.max()), 32) if hasattr(model, 'stride') else 32
         img = torch.zeros((1, model.yaml.get('ch', 3), stride, stride), device=next(model.parameters()).device)  # input
-        flops = profile(deepcopy(model), inputs=(img,), verbose=False)[0] / 1E9 * 2  # stride GFLOPS
+        flops = thop.profile(deepcopy(model), inputs=(img,), verbose=False)[0] / 1E9 * 2  # stride GFLOPS
         img_size = img_size if isinstance(img_size, list) else [img_size, img_size]  # expand if int/float
         fs = ', %.1f GFLOPS' % (flops * img_size[0] / stride * img_size[1] / stride)  # 640x640 GFLOPS
     except (ImportError, Exception):
-        traceback.print_exc()
         fs = ''
 
     logger.info(f"Model Summary: {len(list(model.modules()))} layers, {n_p} parameters, {n_g} gradients{fs}")
